5f4db44c05298f19de358a8e835465ca58a7f879
Shaun Kent <shaunkent81@gmail.com>
2021-07-03 18:33:43 -0700

    add first test and code to pass

---
+++ README.md
@@ -0,0 +1,5 @@
+Describe: beebBoop()
+
+Test: "It should return an array with a 0 if the number 0 is inputted."
+Expect(beebBoop(0).toEqual([0]);
+
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Business logic
+
+function beebBoop(number){
+  let numberArray = [];
+  numberArray.push(number);
+  console.log(numberArray);
+}
+
+beebBoop(0);
